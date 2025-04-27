041b18f6f98708f58d12342e002fad4e51cd7413
Andrej Pedchenko <andrej.cdo@gmail.com>
2025-04-27 15:54:05 +0300

    feature/symfony-skeleton-init
    Symfony init

--- api/public/index.php
+++ api/public/index.php
@@ -2,4 +2,10 @@
 
 declare(strict_types=1);
 
-echo "Hello World!";
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
