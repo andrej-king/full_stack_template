244a8da23f680301eab4e9e026df981752d42e2d
Andrej Pedchenko <andrej.cdo@gmail.com>
2025-04-27 16:32:24 +0300

    feature/symfony-skeleton-init
    Added debug controller

---
+++ api/src/Controller/Debug/Action.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Debug;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use function App\env;
+
+/**
+ * Controller work only with local env
+ */
+final class Action extends AbstractController
+{
+    #[Route(path: '/debug', name: 'test', methods: ['GET'], env: 'local')]
+    public function __invoke(): Response
+    {
+//        phpinfo();
+
+        return new JsonResponse([
+            'msg' => 'debug',
+            'env' => env('APP_ENV')
+        ]);
+    }
+}
